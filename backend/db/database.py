e70aa554f4b1bdfd0840f061f40ffda7fe3eff3a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-29 02:42:01 +0300

    Wrote the database connection code

--- backend/db/database.py
+++ backend/db/database.py
@@ -1,1 +1,21 @@
 """The database connection code"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+DB_URL = "sqlite:///./ig_api.db"
+
+engine = create_engine(DB_URL, connect_args={"check_same_thread": False})
+
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
