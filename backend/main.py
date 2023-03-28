95c34f3c7692c06768abd6a46304ee7e6e652f06
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-29 02:35:16 +0300

    Wrote the root api endpoint

--- backend/main.py
+++ backend/main.py
@@ -3,3 +3,13 @@ from fastapi import FastAPI
 
 
 app = FastAPI()
+
+
+@app.get("/")
+async def root() -> str:
+    """The root API endpoint
+
+    Returns:
+        str: A message is returned after successful execution
+    """
+    return "API works"
