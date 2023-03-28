f196f4d279b985cd647270c85fb2297765524053
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-29 02:43:33 +0300

    Made db conform to snake case naming convention

--- backend/db/database.py
+++ backend/db/database.py
@@ -14,8 +14,8 @@ Base = declarative_base()
 
 
 def get_db():
-    db = SessionLocal()
+    _db = SessionLocal()
     try:
-        yield db
+        yield _db
     finally:
-        db.close()
+        _db.close()
