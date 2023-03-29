e59d43f7a5c7bd9c1544937672f4e758f69d6cff
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-29 04:01:05 +0300

    Took the functions out of the class

--- backend/db/hashing.py
+++ backend/db/hashing.py
@@ -5,28 +5,26 @@ from passlib.context import CryptContext
 password_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
 
-class Hash():
-    """The hashing class"""
+def password_hash(_password: str):
+    """A function meant to hash the password
 
-    def password_hash(self, _password: str):
-        """A function meant to hash the password
+    Args:
+        _password (str): The password
 
-        Args:
-            _password (str): The password
+    Returns:
+        str: The password hash
+    """
+    return password_context.hash(_password)
 
-        Returns:
-            str: The password hash
-        """
-        return password_context.hash(_password)
 
-    def verify_password(self, plain_password: str, hashed_password: str):
-        """A function to verify the passwords after hashing
+def verify_password(plain_password: str, hashed_password: str):
+    """A function to verify the passwords after hashing
 
-        Args:
-            plain_password (str): The plain password
-            hashed_password (str): The passwords after being hashed
+    Args:
+        plain_password (str): The plain password
+        hashed_password (str): The passwords after being hashed
 
-        Returns:
-            bool: Whether they re teh same on not
-        """
-        return password_context.verify(plain_password, hashed_password)
+    Returns:
+        bool: Whether they re teh same on not   
+    """
+    return password_context.verify(plain_password, hashed_password)
