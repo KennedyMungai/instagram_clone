27c069768cbefa9e80d05720aeff312813176a21
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-30 02:54:48 +0300

    Went back to using PostRequest

--- backend/db/db_post.py
+++ backend/db/db_post.py
@@ -1,11 +1,11 @@
 """Determines all the Post interactions with the database"""
 from db.models import Post
-from schemas.post_schema import PostResponse
+from schemas.post_schema import PostRequest
 from sqlalchemy.orm.session import Session
 from datetime import datetime
 
 
-def create_post(_request: PostResponse, _db: Session):
+def create_post(_request: PostRequest, _db: Session):
     new_post = Post(
         mage_url=_request.image_url,
         image_url_type=_request.image_url_type,
