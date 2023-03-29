7aad1ae4c60ac0cc843c643de640bccae8a2c66e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-29 03:15:24 +0300

    Added some imports and created a dummy create_user function

--- backend/db/db_user.py
+++ backend/db/db_user.py
@@ -1,1 +1,7 @@
 """The file that communicates with the database"""
+from sqlalchemy.orm.session import Session
+from schemas.user_schema import UserRequest
+
+
+def create_user(_db: Session, request: UserRequest):
+    pass
