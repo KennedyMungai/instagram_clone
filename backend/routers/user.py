95706b6939fead131825e20f287fafd8102e221f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-29 03:26:40 +0300

    Organized the imports and created the skeleton for the create_user endpoint

--- backend/routers/user.py
+++ backend/routers/user.py
@@ -1,5 +1,14 @@
 """The user route"""
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm.session import Session
+
+from backend.db.database import get_db
+from backend.schemas.user_schema import UserRequest, UserResponse
 
 
 user_router = APIRouter(prefix="/user", tags=["User"])
+
+
+@user_router.get("/", response_model=UserResponse)
+async def create_user(_request: UserRequest, _db: Session = Depends(get_db)):
+    pass
