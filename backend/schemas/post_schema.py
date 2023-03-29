44ff915190387299e3c9b5a9dd696c421ce2a1c0
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-29 04:14:16 +0300

    Created the PostBase class

--- backend/schemas/post_schema.py
+++ backend/schemas/post_schema.py
@@ -1,1 +1,14 @@
 """The post schema file"""
+from pydantic import BaseModel
+
+
+class PostBase(BaseModel):
+    """The base class for the Post data
+
+    Args:
+        BaseModel (Class): The parent claSS
+    """
+    image_url: str
+    image_url_type: str
+    caption: str
+    creator_id: int
