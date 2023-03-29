008101ba9e74a434c5ed39d2fa9ee8ab7da5a0ac
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-30 02:45:32 +0300

    Imported the Post model

--- backend/db/db_post.py
+++ backend/db/db_post.py
@@ -1,1 +1,8 @@
 """Determines all the Post interactions with the database"""
+from schemas.post_schema import PostRequest
+from sqlalchemy.orm.session import Session
+from db.models import Post
+
+
+def create_post(_request: PostRequest, _db: Session):
+    pass
