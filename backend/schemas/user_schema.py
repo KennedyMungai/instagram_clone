98af255dc9aa13a0d06b088d71c652e30f98f8c6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-29 03:11:00 +0300

    Created the UserBase and UserResponse schemas

--- backend/schemas/user_schema.py
+++ backend/schemas/user_schema.py
@@ -1,2 +1,23 @@
 """The file will hold the schema logic for the user"""
 from pydantic import BaseModel
+
+
+class UserBase(BaseModel):
+    """The base model for the user data
+
+    Args:
+        BaseModel (Class): The parent class
+    """
+    username: str
+    email: str
+    password: str
+
+
+class UserResponse(BaseModel):
+    """The template for user response data
+
+    Args:
+        BaseModel (Class): The parent class
+    """
+    username: str
+    email: str
