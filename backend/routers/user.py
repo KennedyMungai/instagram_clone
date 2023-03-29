632686bfe04f344becfef6a47e713adbdd560454
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-29 03:28:29 +0300

    Spaced out the parameters for the api endpoint

--- backend/routers/user.py
+++ backend/routers/user.py
@@ -9,6 +9,12 @@ from backend.schemas.user_schema import UserRequest, UserResponse
 user_router = APIRouter(prefix="/user", tags=["User"])
 
 
-@user_router.get("/", response_model=UserResponse)
-async def create_user(_request: UserRequest, _db: Session = Depends(get_db)):
+@user_router.get(
+    "/",
+    response_model=UserResponse
+)
+async def create_user(
+    _request: UserRequest,
+    _db: Session = Depends(get_db)
+):
     pass
