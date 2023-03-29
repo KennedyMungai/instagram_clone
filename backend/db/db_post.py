2f5419e0e7f5fc6a9231b93f79264821029460bc
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-30 02:53:12 +0300

    Finished writing teh create_post function

--- backend/db/db_post.py
+++ backend/db/db_post.py
@@ -1,8 +1,15 @@
 """Determines all the Post interactions with the database"""
 from db.models import Post
-from schemas.post_schema import PostRequest
+from schemas.post_schema import PostResponse
 from sqlalchemy.orm.session import Session
+from datetime import datetime
 
 
-def create_post(_request: PostRequest, _db: Session):
-    pass
+def create_post(_request: PostResponse, _db: Session):
+    new_post = Post(
+        mage_url=_request.image_url,
+        image_url_type=_request.image_url_type,
+        caption=_request.caption,
+        timestamp=datetime.utcnow()
+        user_id=_request.creator_id
+    )
