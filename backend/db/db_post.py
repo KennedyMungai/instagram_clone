459a625ade745d849d651e474bf6121aae6aaffb
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-30 02:58:18 +0300

    Added a docstring to the create_poost function

--- backend/db/db_post.py
+++ backend/db/db_post.py
@@ -7,6 +7,15 @@ from sqlalchemy.orm.session import Session
 
 
 def create_post(_request: PostRequest, _db: Session):
+    """A function that creates a new post
+
+    Args:
+        _request (PostRequest): The template for the Post request data
+        _db (Session): The database session
+
+    Returns:
+        PostRequest: Te new post
+    """
     _new_post = Post(
         mage_url=_request.image_url,
         image_url_type=_request.image_url_type,
