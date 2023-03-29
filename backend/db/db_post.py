3b84dfa12ea35887436893e9a279434490c2e7cf
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-30 02:46:28 +0300

    Sorte dthe imports

--- backend/db/db_post.py
+++ backend/db/db_post.py
@@ -1,7 +1,7 @@
 """Determines all the Post interactions with the database"""
+from db.models import Post
 from schemas.post_schema import PostRequest
 from sqlalchemy.orm.session import Session
-from db.models import Post
 
 
 def create_post(_request: PostRequest, _db: Session):
