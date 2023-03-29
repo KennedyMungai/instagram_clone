5dd9e64f758496f82a3f29aef3a6f49b99fbc105
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-29 03:18:33 +0300

    Added a docstring to the create user function

--- backend/db/db_user.py
+++ backend/db/db_user.py
@@ -1,7 +1,18 @@
 """The file that communicates with the database"""
-from sqlalchemy.orm.session import Session
 from schemas.user_schema import UserRequest
+from sqlalchemy.orm.session import Session
+from db.models import User
 
 
-def create_user(_db: Session, request: UserRequest):
-    pass
+def create_user(_db: Session, _request: UserRequest):
+    """The create user function
+
+    Args:
+        _db (Session): The database session
+        _request (UserRequest): The template for the user request data
+    """
+    new_user = User(
+        username=_request.username,
+        email=_request.email,
+        password=_request.password
+    )
