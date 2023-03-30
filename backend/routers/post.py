043224b81ce583cb982826fc8a034499d3f41c86
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-30 03:07:41 +0300

    Sorted the imports

--- backend/routers/post.py
+++ backend/routers/post.py
@@ -1,8 +1,15 @@
 """The Post router file"""
-from fastapi import APIRouter
-
+from db.database import get_db
+from fastapi import APIRouter, Depends, status
+from schemas.post_schema import PostRequest
+from sqlalchemy.orm.session import Session
 
 post_router = APIRouter(prefix="/post", tags=["Post"])
 
 
 image_url_types = ['absolute', 'relative']
+
+
+@post_router.post("/", status_code=status.HTTP_201_CREATED)
+async def create_new_post(_request: PostRequest, _db: Session = Depends(get_db)):
+    pass
