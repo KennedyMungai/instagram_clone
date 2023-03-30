cfb2c0fef7bc343faefb1775b809e4cb15e6c3cd
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-30 03:11:39 +0300

    Raised an exception for an invalid image_url_type prop

--- backend/routers/post.py
+++ backend/routers/post.py
@@ -1,6 +1,6 @@
 """The Post router file"""
 from db.database import get_db
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, status, HTTPException
 from schemas.post_schema import PostRequest
 from sqlalchemy.orm.session import Session
 
@@ -12,4 +12,8 @@ image_url_types = ['absolute', 'relative']
 
 @post_router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_new_post(_request: PostRequest, _db: Session = Depends(get_db)):
-    pass
+    if not _request.image_url_type in image_url_types:
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail="Parameter image url type can only take the values 'absolute' or 'relative'"
+        )
